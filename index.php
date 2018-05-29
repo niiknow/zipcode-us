70056f6a20269c24e16116c6897cd27ddf7ed09d
Tom Noogen <tom@noogen.net>
2018-05-28 23:27:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
-// retrieve the table and key from the path
 $zipcode = preg_replace('/[^0-9]/i','',$_SERVER['REQUEST_URI']);
+if (!isset($zipcode) || strlen($zipcode) != 5) {
+    return;
+}
 $zip2 = substr($zipcode, 0, 2);
 $rst = file_get_contents('./db/' . $zip2 . '/' . $zipcode . '.json');
 $callback = $_GET['callback'];
 
-header('Content-type: application/json; charset=utf-8'); 
 header("access-control-allow-origin: *");  
 if (isset($callback))
 {
+  header('Content-type: application/javascript; charset=utf-8'); 
   echo $callback . '(' . $rst . ');';
   return;
 }
 
+header('Content-type: application/json; charset=utf-8'); 
 echo $rst;
 ?>
